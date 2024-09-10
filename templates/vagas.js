60be5fad1ed6d5baf76529c0b098cbf61ff70f62
Fabio Castanho <fabiocastanho12@gmail.com>
2024-09-10 12:00:51 +0100

    criação do serviço

--- templates/vagas.js
+++ templates/vagas.js
@@ -1,23 +1,23 @@
-document.getElementById('filterButton').addEventListener('click', function() {
-    const location = document.getElementById('location').value.toLowerCase();
-    const jobType = document.getElementById('jobType').value.toLowerCase();
-    const search = document.getElementById('search').value.toLowerCase();
+document.getElementById("filterButton").addEventListener("click", function () {
+  const location = document.getElementById("location").value.toLowerCase();
+  const jobType = document.getElementById("jobType").value.toLowerCase();
+  const search = document.getElementById("search").value.toLowerCase();
 
-    const jobs = document.querySelectorAll('.job-item');
+  const jobs = document.querySelectorAll(".job-item");
 
-    jobs.forEach(job => {
-        const jobLocation = job.getAttribute('data-location').toLowerCase();
-        const jobTypeAttr = job.getAttribute('data-type').toLowerCase();
-        const jobTitle = job.querySelector('h2').textContent.toLowerCase();
+  jobs.forEach((job) => {
+    const jobLocation = job.getAttribute("data-location").toLowerCase();
+    const jobTypeAttr = job.getAttribute("data-type").toLowerCase();
+    const jobTitle = job.querySelector("h2").textContent.toLowerCase();
 
-        let locationMatch = location === '' || jobLocation === location;
-        let typeMatch = jobType === '' || jobTypeAttr === jobType;
-        let searchMatch = jobTitle.includes(search);
+    let locationMatch = location === "" || jobLocation === location;
+    let typeMatch = jobType === "" || jobTypeAttr === jobType;
+    let searchMatch = jobTitle.includes(search);
 
-        if (locationMatch && typeMatch && searchMatch) {
-            job.style.display = 'block';
-        } else {
-            job.style.display = 'none';
-        }
-    });
+    if (locationMatch && typeMatch && searchMatch) {
+      job.style.display = "block";
+    } else {
+      job.style.display = "none";
+    }
+  });
 });
